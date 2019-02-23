7c281a18bfe5cad2f523d41d2e0090ff32e66709
mariomrtsnz <mario.mrtsnz@gmail.com>
2019-02-23 13:29:55 +0100

    fixed .env gitignore. Implemented android material

--- android/app/src/main/java/com/mario/homely/LoginActivity.java
+++ android/app/src/main/java/com/mario/homely/LoginActivity.java
@@ -3,18 +3,13 @@ package com.mario.homely;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.app.LoaderManager.LoaderCallbacks;
-
 import android.content.CursorLoader;
 import android.content.Loader;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
-
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
@@ -29,8 +24,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.material.snackbar.Snackbar;
+
 import java.util.ArrayList;
 import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import static android.Manifest.permission.READ_CONTACTS;
 
