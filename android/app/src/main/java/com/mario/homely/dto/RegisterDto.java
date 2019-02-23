a7c4be3b672ffd961369a6114cd2bde1839d3e95
mariomrtsnz <mario.mrtsnz@gmail.com>
2019-02-23 23:58:31 +0100

    Added registerdto, loginresponse and loginservice

--- android/app/src/main/java/com/mario/homely/dto/RegisterDto.java
+++ android/app/src/main/java/com/mario/homely/dto/RegisterDto.java
@@ -1,4 +1,75 @@
 package com.mario.homely.dto;
 
 public class RegisterDto {
+    private String name;
+    private String email;
+    private String language;
+    private String password;
+    private String picture;
+    private String role;
+
+    public RegisterDto() {
+    }
+
+    public RegisterDto(String name, String email, String password, String picture, String role) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.picture = picture;
+        this.role = role;
+    }
+
+    public RegisterDto( String email, String password, String language) {
+        this.email = email;
+        this.password = password;
+        this.language = language;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
